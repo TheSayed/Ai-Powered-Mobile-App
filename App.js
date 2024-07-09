21c51347db7c95f4150fc2bf2772193c535c1a1e
TheSayed <kotp9a@gmail.com>
2024-07-09 12:27:46 +0300

    fonts

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
+import "react-native-url-polyfill/auto";
+import React, { useCallback } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import MainNavigator from "./src/navigators/MainNavigator.js";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    regular: require("./assets/fonts/Poppins-Regular.ttf"),
+    bold: require("./assets/fonts/Poppins-Bold.ttf"),
+    boldItalic: require("./assets/fonts/Poppins-BlackItalic.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
   return (
-    <NavigationContainer>
-      <MainNavigator />
-    </NavigationContainer>
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <NavigationContainer>
+        <MainNavigator />
+      </NavigationContainer>
+    </View>
   );
 }
 
