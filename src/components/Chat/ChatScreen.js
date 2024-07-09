bf343d492fa0c41496dab0f1c7fb80ea19d5ebe0
TheSayed <kotp9a@gmail.com>
2024-07-09 12:28:22 +0300

    chat bubble

--- src/components/Chat/ChatScreen.js
+++ src/components/Chat/ChatScreen.js
@@ -1,26 +1,76 @@
+import React, { useCallback, useEffect, useState } from "react";
 import {
   View,
   StyleSheet,
   TextInput,
   TouchableOpacity,
   KeyboardAvoidingView,
+  FlatList,
+  Text,
 } from "react-native";
-import React, { useState } from "react";
 import colors from "../../constants/colors.js";
 import { Feather } from "@expo/vector-icons";
+import {
+  addUserMessage,
+  getConversation,
+  initConversation,
+} from "../../../utilis/conversationHistory.js";
+import { checkUsage, makeChatRequest } from "../../../utilis/gbtUtilis.js";
+import BubbleChat from "./BubbleChat.js";
 
 const ChatScreen = () => {
-  const [messages, setMessages] = useState("");
+  const [messageText, setMessageText] = useState("");
+  const [conversation, setConversation] = useState([]);
+
+  useEffect(() => {
+    initConversation();
+    setConversation([]);
+  }, []);
+
+  const sendMessage = useCallback(async () => {
+    if (messageText === "") return;
+    try {
+      addUserMessage(messageText);
+      setMessageText("");
+      setConversation(getConversation());
+
+      await makeChatRequest();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setConversation(getConversation());
+    }
+  }, [messageText]);
+
   return (
     <KeyboardAvoidingView
       style={styles.container}
       behavior="padding"
       keyboardVerticalOffset={100}
     >
-      <View style={styles.messagesContainer}></View>
+      <View style={styles.messagesContainer}>
+        <FlatList
+          data={conversation}
+          renderItem={(itemData) => {
+            const conversationItem = itemData.item;
+            if (conversationItem.role === "system") return null;
+            return (
+              <BubbleChat
+                text={conversationItem.content}
+                role={conversationItem.role}
+              />
+            );
+          }}
+        />
+      </View>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textBox} placeholder="Type a message...." />
-        <TouchableOpacity style={styles.sendButton}>
+        <TextInput
+          style={styles.textBox}
+          placeholder="Type a message...."
+          value={messageText}
+          onChangeText={setMessageText}
+        />
+        <TouchableOpacity style={styles.sendButton} onPress={sendMessage}>
           <Feather name="send" size={24} color="white" />
         </TouchableOpacity>
       </View>
@@ -48,9 +98,12 @@ const styles = StyleSheet.create({
   },
   textBox: {
     flex: 1,
+    fontFamily: "regular",
+    letterSpacing: 0.5,
   },
   messagesContainer: {
     flex: 1,
+    padding: 12,
   },
 });
 
