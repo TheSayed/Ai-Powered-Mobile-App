4638591774ac39b56a4abc38e7a24dd5fca9e2ee
TheSayed <kotp9a@gmail.com>
2024-07-17 15:59:53 +0300

    fonts

--- App.js
+++ App.js
@@ -1,12 +1,64 @@
-import "react-native-url-polyfill/auto";
+// App.js
+import "react-native-gesture-handler";
 import React, { useCallback } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import MainNavigator from "./src/navigators/MainNavigator.js";
-import { useFonts } from "expo-font";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from "expo-font";
+import { Fontisto, Entypo } from "@expo/vector-icons";
+import ChatScreen from "./src/components/Chat/ChatScreen";
+import ImageScreen from "./src/components/Image/ImageScreen";
+import SettingsScreen from "./src/components/Settings/SettingsScreen";
 
 SplashScreen.preventAutoHideAsync();
+
+const Tab = createBottomTabNavigator();
+
+const MainNavigator = () => {
+  const options = {
+    headerTitleStyle: {
+      fontFamily: "regular",
+    },
+    tabBarLabelStyle: {
+      fontFamily: "regular",
+    },
+  };
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Chat"
+        component={ChatScreen}
+        options={{
+          ...options,
+          tabBarIcon: ({ color, size }) => (
+            <Entypo name="chat" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          ...options,
+          tabBarIcon: ({ color, size }) => (
+            <Fontisto name="player-settings" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Image"
+        component={ImageScreen}
+        options={{
+          ...options,
+          tabBarIcon: ({ color, size }) => (
+            <Entypo name="image-inverted" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 
 export default function App() {
   const [fontsLoaded] = useFonts({
